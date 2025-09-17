a707bbcbd053db8f8ea590d566aaa4e195cedf37
Hamdi Almutairi <github@alareij.com>
2025-09-17 20:21:51 +0300

    Refactor main function to include lock file creation and handling

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # main.py
 
+import sys
 from src.nasikh import Nasikh
+from PySide6.QtCore import QLockFile, QDir
+
 
 def main():
     """Main function to run the Dictation Application."""
+    LOCK_FILE_NAME = "nasikh.lock"
+    # Create a lock file in the system's temporary directory
+    lock_file_path = QDir.tempPath() + "/" + LOCK_FILE_NAME
+    lock_file = QLockFile(lock_file_path)
+
+    # Attempt to lock the file
+    if not lock_file.lock():
+        sys.exit(0)
+    
     dictation = Nasikh()
     dictation.run()
 
